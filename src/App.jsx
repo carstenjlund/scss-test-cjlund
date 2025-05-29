606c2012d153289222419b2e34f9695d595ff4cb
Carsten <clu@rts.dk>
2025-05-29 14:23:40 +0200

    updated imports in  app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header'
+import Header from '@/components/header.jsx'
+import '@/scss/main.scss' // Import global styles
 
 function App() {
   const [count, setCount] = useState(0)
